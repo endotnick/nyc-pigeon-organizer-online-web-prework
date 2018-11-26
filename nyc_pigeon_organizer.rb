3a7f181a06db4005d5aa4a8185ddbca1a2b2b395
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-11-26 05:09:25 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,15 +3,15 @@ def nyc_pigeon_organizer(data)
   data.each do |attribute, values|
     values.each do |value, pigeons|
       pigeons.each do |pigeon|
-        if !pigeon_list[pigeon]
+        if !pigeon_list.has_key?(pigeon)
           pigeon_list[pigeon] = {}
         end
 
-        if !pigeon_list[pigeon][attribute]
+        if !pigeon_list[pigeon].has_key?(attribute)
           pigeon_list[pigeon][attribute] = []
         end
 
-        if !pigeon_list[pigeon][attribute][value]
+        if !pigeon_list[pigeon][attribute].has_key?(value)
           pigeon_list[pigeon][attribute] << value.to_s
         end
       end
